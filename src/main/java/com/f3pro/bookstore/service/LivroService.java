5b53a0367ff52b847e3037ced6a2ed5de2e4b205
Francivaldo.souza <francivaldo.alves@gmail.com>
2021-01-22 20:47:10 -0300

    livro buscar por ai

---
+++ src/main/java/com/f3pro/bookstore/service/LivroService.java
@@ -0,0 +1,22 @@
+package com.f3pro.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.f3pro.bookstore.domain.Livro;
+import com.f3pro.bookstore.repositories.LivroRepository;
+import com.f3pro.bookstore.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class LivroService {
+	@Autowired
+	private LivroRepository livroRepository;
+
+	public Livro findById(Integer id) {
+		Optional<Livro> obj = livroRepository.findById(id);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Livro não localizado! id: " + id + ", Tipo: " + Livro.class.getName()));
+	}
+}
