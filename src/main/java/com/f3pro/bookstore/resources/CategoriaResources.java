30ab4ad35c345576917d26a2836a697f735e49c9
Francivaldo.souza <francivaldo.alves@gmail.com>
2021-01-22 19:39:34 -0300

    Delete para categotia

--- src/main/java/com/f3pro/bookstore/resources/CategoriaResources.java
+++ src/main/java/com/f3pro/bookstore/resources/CategoriaResources.java
@@ -17,38 +17,48 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
 
-    @Autowired
-    private CategoriaService service;
+	@Autowired
+	private CategoriaService service;
 
-    // localhost:8080/categorias/1
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<Categoria> findByid(@PathVariable Integer id) {
-        Categoria obj = service.findById(id);
-        return ResponseEntity.ok().body(obj);
+	// localhost:8080/categorias/1
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Categoria> findByid(@PathVariable Integer id) {
+		Categoria obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 
+	}
+
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoriaDTO> update(@PathVariable Integer id, @RequestBody Categoria objTDO) {
+		Categoria newObj = service.update(id, objTDO);
+
+		return ResponseEntity.ok().body(new CategoriaDTO(newObj));
+
+	}
+
+	@DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+    	service.delete(id);
+    	
+    	return ResponseEntity.noContent().build();
+   
     }
-
-    @GetMapping
-    public ResponseEntity<List<CategoriaDTO>> findAll() {
-        List<Categoria> list = service.findAll();
-        List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
-        return ResponseEntity.ok().body(listDTO);
-    }
-
-    @PostMapping
-    public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
-        obj = service.create(obj);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(obj.getId()).toUri();
-        return ResponseEntity.created(uri).build();
-    }
-
-    @PutMapping(value = "/{id}")
-    public ResponseEntity<CategoriaDTO> update(@PathVariable Integer id, @RequestBody Categoria objTDO) {
-        Categoria newObj = service.update(id, objTDO);
-
-        return ResponseEntity.ok().body(new CategoriaDTO(newObj));
-
-
-    }
+    
+    
+    
 
 }
--- src/main/java/com/f3pro/bookstore/service/CategoriaService.java
+++ src/main/java/com/f3pro/bookstore/service/CategoriaService.java
@@ -38,4 +38,10 @@ public class CategoriaService {
         return repository.save(obj);
 
     }
+
+	public void delete(Integer id) {
+		findById(id);
+		repository.deleteById(id);
+		
+	}
 }
